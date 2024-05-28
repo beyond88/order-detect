6833e211c88f26c14f878ecdb201802289f2c499
beyond88 <muhin.cse.diu@gmail.com>
2024-05-28 16:19:28 +0600

    set per page

--- includes/Admin/MultipleOrderTracking.php
+++ includes/Admin/MultipleOrderTracking.php
@@ -28,6 +28,8 @@ class MultipleOrderTracking
     {
         $this->main = $main;
         add_filter('ordershield_admin_menu', array($this, 'ordershield_multiple_order_tracking'), PHP_INT_MAX);
+        add_filter('set-screen-option', array($this, 'set_screen_option'), 10, 3);
+        add_action("load-ordershield_page_multiple-order-tracking", array($this, 'add_screen_options'));
     }
 
     /**
@@ -41,18 +43,56 @@ class MultipleOrderTracking
      */
     public function ordershield_multiple_order_tracking($settings)
     {
-        $settings['multiple']['parent_slug'] = 'order-shield';
-        $settings['multiple']['page_title'] = __('Multiple Order Tracking', 'order-shield');
-        $settings['multiple']['menu_title'] = __('Multiple Order Tracking', 'order-shield');
-        $settings['multiple']['capability'] = 'manage_options';
-        $settings['multiple']['callback'] = function ($arg) {
+        $settings['multiple-order-tracking']['parent_slug'] = 'order-shield';
+        $settings['multiple-order-tracking']['page_title'] = __('Multiple Order Tracking', 'order-shield');
+        $settings['multiple-order-tracking']['menu_title'] = __('Multiple Order Tracking', 'order-shield');
+        $settings['multiple-order-tracking']['capability'] = 'manage_options';
+        // Instantiate the class
+        $phone_number = isset($_POST['customer_phone']) ? sanitize_text_field($_POST['customer_phone']) : '';
+        $orders_list_table = new MultipleOrderTrackingList($phone_number);
+        $orders_list_table->prepare_items();
+
+        $settings['multiple-order-tracking']['callback'] = function () use ($orders_list_table, $phone_number) {
             $template = __DIR__ . '/views/multiple-order-tracking.php';
 
+            // Pass variables to the template
             if (file_exists($template)) {
                 include $template;
             }
         };
 
         return $settings;
+    }
+
+    /**
+     * Validate screen options on update.
+     *
+     * @param $status Screen option value. Default false to skip.
+     * @param $option The option name.
+     * @param $value The number of rows to use.
+     * @return bool|int
+     */
+    public function set_screen_option($status, $option, $value)
+    {
+        if (in_array($option, array('multi_order_tracking_per_page'), true)) {
+            return $value;
+        }
+
+        return $status;
+    }
+
+    /**
+     * Load outbox page assets
+     */
+    public function add_screen_options()
+    {
+        /**
+         * Add per page option.
+         */
+        add_screen_option('per_page', array(
+            'label'   => esc_html__('Number of items per page', 'order-shield'),
+            'default' => 20,
+            'option'  => 'multi_order_tracking_per_page',
+        ));
     }
 }
--- includes/Admin/views/multiple-order-tracking.php
+++ includes/Admin/views/multiple-order-tracking.php
@@ -1,18 +1,15 @@
-<?php //$phone_number = isset($_POST['customer_phone']) ? sanitize_text_field($_POST['customer_phone']) : ''; 
+<?php $phone_number = isset($_POST['customer_phone']) ? sanitize_text_field($_POST['customer_phone']) : '';
 ?>
 
 <div class="wrap">
-    <h1>Multiple Order Tracking</h1>
+    <h1 class="wp-heading-inline"><?php echo __('Multiple Order Tracking', 'order-shield'); ?></h1>
     <form method="post" action="">
-        <input type="text" name="customer_phone" placeholder="Enter customer phone number" value="<?php //echo esc_attr($phone_number); 
-                                                                                                    ?>" required>
+        <input type="text" name="customer_phone" placeholder="Enter customer phone number" value="<?php echo esc_attr($phone_number); ?>" required>
         <input type="submit" name="search_orders" value="Search Orders" class="button-primary">
     </form>
     <?php
-    // if (!empty($phone_number)) {
-    //     $orders_list_table = new MultipleOrderTrackingList($phone_number);
-    //     $orders_list_table->prepare_items();
-    //     $orders_list_table->display();
-    // }
+    if ($orders_list_table) {
+        $orders_list_table->display();
+    }
     ?>
 </div>
